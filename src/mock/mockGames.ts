6a36db14e57605e5c3f9a973ffcf955979c8ab5a
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-12-08 16:32:43 +0200

    create searchbar and display mock games at catalog page

---
+++ src/mock/mockGames.ts
@@ -0,0 +1,12 @@
+export const mockGames = [
+  { id: 1, name: 'Elden Ring', rating: 9.7 },
+  { id: 2, name: 'Hollow Knight', rating: 9.3 },
+  { id: 3, name: 'Celeste', rating: 9.5 },
+  { id: 4, name: 'The Witcher 3', rating: 9.8 },
+  { id: 5, name: 'The Legend of Zelda: Breath of the Wild', rating: 9.8 },
+  { id: 6, name: 'Cyberpunk 2077', rating: 9.8 },
+  { id: 7, name: 'Rise of the Tomb Raider', rating: 9.8 },
+  { id: 8, name: 'Lies of P', rating: 9.8 },
+  { id: 9, name: 'Half Life 2', rating: 9.8 },
+  { id: 10, name: 'Bioshock', rating: 9.8 },
+];
