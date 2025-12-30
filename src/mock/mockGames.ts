f572627938e3008612a12c61ffd3ac5737310db1
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-12-30 18:12:05 +0200

    create profile page mobile layout

--- src/mock/mockGames.ts
+++ src/mock/mockGames.ts
@@ -87,28 +87,32 @@ export const popularGames = [
     name: 'S.T.A.L.K.E.R. 2: Heart of Chornobyl',
     year: 2024,
     apiRating: 7.9,
-    backgroundImage: '/images/stalker.jpg',
+    backgroundImage:
+      'https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg',
   },
   {
     apiId: 1,
     name: 'Shadow of the Colossus',
     year: 2006,
     apiRating: 9.2,
-    backgroundImage: '/images/colossus.jpg',
+    backgroundImage:
+      'https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg',
   },
   {
     apiId: 1,
     name: 'Metroid Prime 4: Beyond',
     year: 2025,
     apiRating: 9.5,
-    backgroundImage: '/images/metroid.jpg',
+    backgroundImage:
+      'https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg',
   },
   {
     apiId: 1,
     name: 'Ghost of Yotei',
     year: 2025,
     apiRating: 7.5,
-    backgroundImage: '/images/ghost.jpg',
+    backgroundImage:
+      'https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg',
   },
 ];
 
@@ -118,27 +122,31 @@ export const newGames = [
     name: 'Anno 117: Pax Romana',
     year: 2025,
     apiRating: 8.3,
-    backgroundImage: '/images/anno.jpg',
+    backgroundImage:
+      'https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg',
   },
   {
     apiId: 1,
     name: 'Metal Gear Solid & Snake Eater',
     year: 2025,
     apiRating: 7.4,
-    backgroundImage: '/images/mgs.jpg',
+    backgroundImage:
+      'https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg',
   },
   {
     apiId: 1,
     name: 'Keeper',
     year: 2025,
     apiRating: 6.7,
-    backgroundImage: '/images/keeper.jpg',
+    backgroundImage:
+      'https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg',
   },
   {
     apiId: 1,
     name: 'ARC Raiders',
     year: 2025,
     apiRating: 7.8,
-    backgroundImage: '/images/arc.jpg',
+    backgroundImage:
+      'https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg',
   },
 ];
